464cf94f43a8e65f46e977a63a84c64d6e7f5015
August Money <10575396+sudo-August@users.noreply.github.com>
2021-02-13 15:25:14 -0600

    completed 1

--- contracts/AssociateProfitSplitter.sol
+++ contracts/AssociateProfitSplitter.sol
@@ -1,13 +1,16 @@
-pragma solidity ^0.5.0;
+pragma solidity >0.5.0 <0.9.0;
 
 // lvl 1: equal split
 contract AssociateProfitSplitter {
     // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    address payable public employeeOne;
+    address payable public employeeTwo;
+    address payable public employeeThree;
 
     constructor(address payable _one, address payable _two, address payable _three) public {
-        employee_one = _one;
-        employee_two = _two;
-        employee_three = _three;
+        employeeOne = _one;
+        employeeTwo = _two;
+        employeeThree = _three;
     }
 
     function balance() public view returns(uint) {
@@ -15,18 +18,19 @@ contract AssociateProfitSplitter {
     }
 
     function deposit() public payable {
-        // @TODO: Split `msg.value` into three
-        uint amount = ; // Your code here!
+        uint amount = msg.value / 3;
 
         // @TODO: Transfer the amount to each employee
-        // Your code here!
+        employeeOne.transfer(amount);
+        employeeTwo.transfer(amount);
+        employeeThree.transfer(amount);
 
         // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+        msg.sender.transfer(msg.value - amount * 3);
     }
 
     function() external payable {
         // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
+        deposit();
     }
 }
